1ed87a7c5fd57812fb0b193f1d824767b00013f6
atns <antonislemnos@hotmail.com>
2024-06-17 23:36:15 +0300

    first

---
+++ src/main/java/com/atns/fullstackatns/FullstackAtnsApplication.java
@@ -0,0 +1,13 @@
+package com.atns.fullstackatns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackAtnsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullstackAtnsApplication.class, args);
+    }
+
+}
